5e95d62513fef478cec8e87a63e26066a9870b5b
Kevin Choe <kevinkw.choe@gmail.com>
2020-05-08 15:44:41 -0700

    Create Query-8-analyze-best-sales-agent-annual-performance.sql

---
+++ Project 3: Query a Digital Music Store Database/SQL Queries/Query-8-analyze-best-sales-agent-annual-performance.sql
@@ -0,0 +1,47 @@
+WITH best_sales_agent
+AS (SELECT
+  e.FirstName,
+  e.LastName,
+  CASE
+    WHEN COUNT(*) IS NULL
+    THEN "0"
+    ELSE COUNT(*)
+  END AS assists
+FROM Employee e
+JOIN Customer c
+  ON e.EmployeeId = c.SupportRepId
+WHERE e.Title = "Sales Support Agent"
+GROUP BY 1,
+         2
+ORDER BY 3 DESC
+LIMIT 1)
+
+SELECT
+  e.FirstName,
+  e.LastName,
+  STRFTIME('%Y', i.InvoiceDate) AS year,
+  COUNT(*) AS sales
+FROM Employee e
+JOIN Customer c
+  ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i
+  ON c.CustomerId = i.CustomerId
+JOIN best_sales_agent bsa
+  ON e.FirstName = bsa.FirstName
+  AND e.LastName = bsa.LastName
+GROUP BY 1,
+         2,
+         3
+ORDER BY 3,
+         1,
+         2;
+
+/*
+Output:
+FirstName   LastName   ord_date  Sales
+Jane	      Peacock	   2009	     25
+Jane	      Peacock	   2010	     34
+Jane	      Peacock	   2011	     28
+Jane	      Peacock	   2012	     28
+Jane	      Peacock	   2013	     31
+*/
